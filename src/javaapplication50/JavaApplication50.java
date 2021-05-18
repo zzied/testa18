d590cff5750bc617e4ed4b083b07a03c7334f5c0
Karine Rock <karinerockdesign@live.ca>
2021-05-18 12:01:26 -0400

    Je viens d'ajouter mon nom!!!

--- src/javaapplication50/JavaApplication50.java
+++ src/javaapplication50/JavaApplication50.java
@@ -7,7 +7,7 @@ public class JavaApplication50 {
  
     public static void main(String[] args) {
         System.out.println("Zied Zaier");
-        
+        System.out.println("Karine Rock!");
     }
     
 }
